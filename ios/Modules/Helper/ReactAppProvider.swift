b2c8410f9a7094ed539ba41134a3700443693c98
vadymv-mendix <vadym.vakhovskiy@mendix.com>
2026-03-05 14:51:30 +0100

    chore: fix broken APIs on iOS

--- ios/Modules/Helper/ReactAppProvider.swift
+++ ios/Modules/Helper/ReactAppProvider.swift
@@ -1,28 +1,41 @@
 import UIKit
 import React
 import React_RCTAppDelegate
-import ReactAppDependencyProvider
 
-open class ReactAppProvider: RCTAppDelegate {
+open class ReactAppProvider: RCTDefaultReactNativeFactoryDelegate, UIApplicationDelegate {
     
     public static let defaultName = "App"
+
+    public var window: UIWindow?
+    public var reactNativeFactory: RCTReactNativeFactory?
+    public var moduleName: String = defaultName
     
     var reactRootViewName: String = defaultName
     
-    public func setUpProvider(moduleName: String = ReactAppProvider.defaultName, reactRootViewName: String = ReactAppProvider.defaultName) {
+    public func setUpProvider(
+        moduleName: String = ReactAppProvider.defaultName,
+        reactRootViewName: String = ReactAppProvider.defaultName,
+        dependencyProvider: (any RCTDependencyProvider)? = nil
+    ) {
         self.moduleName = moduleName
         self.reactRootViewName = reactRootViewName
-        automaticallyLoadReactNativeWindow = false
-        dependencyProvider = RCTAppDependencyProvider()
+        if let dependencyProvider {
+            self.dependencyProvider = dependencyProvider
+        }
         window = MendixReactWindow(frame: UIScreen.main.bounds)
+        reactNativeFactory = RCTReactNativeFactory(delegate: self)
     }
-    
-    public override func sourceURL(for bridge: RCTBridge) -> URL? {
-        return self.bundleURL()
+
+    open func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        true
     }
     
     open override func bundleURL() -> URL? {
         return ReactNative.shared.bundleURL()
+    }
+
+    open override func sourceURL(for bridge: RCTBridge) -> URL? {
+        return self.bundleURL()
     }
     
     public func setReactViewController(_ controller: UIViewController) {
@@ -31,9 +44,11 @@ open class ReactAppProvider: RCTAppDelegate {
     }
     
     public func reactAppView() -> UIView? {
-        let view = rootViewFactory().view(withModuleName: reactRootViewName)
+        guard let view = reactNativeFactory?.rootViewFactory.view(withModuleName: reactRootViewName) else {
+            return nil
+        }
         view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        view.frame = window.rootViewController?.view.frame ?? .zero
+        view.frame = window?.rootViewController?.view.frame ?? .zero
         return view
     }
     
@@ -53,12 +68,12 @@ open class ReactAppProvider: RCTAppDelegate {
     }
     
     public func changeRoot(to controller: UIViewController) {
-        window.rootViewController = controller
-        window.makeKeyAndVisible()
+        window?.rootViewController = controller
+        window?.makeKeyAndVisible()
     }
     
     public var rootView: UIView? {
-        return window.rootViewController?.view
+        return window?.rootViewController?.view
     }
     
     public static func getModule<T: NSObject>(type: T.Type) -> T? {
