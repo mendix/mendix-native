cf523b2ed9b946c396ba4e403653055848b6a1ac
Yogendra Shelke <25844542+YogendraShelke@users.noreply.github.com>
2026-09-04 15:19:28 +0530

    feat: migrate to scene based lifecycle

---
+++ example/ios/MendixNativeExample/SceneDelegate.swift
@@ -0,0 +1,39 @@
+import UIKit
+import React
+import MendixNative
+
+class SceneDelegate: ReactAppProvider {
+
+    override func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        super.scene(scene, willConnectTo: session, options: connectionOptions)
+
+        setUpProvider()
+        guard let bundleUrl = bundleURL() else {
+            let message = "No script URL provided. Make sure the metro packager is running or you have embedded a JS bundle in your application bundle."
+            fatalError(message)
+        }
+        let mendixApp = MendixApp.init(
+            identifier: nil,
+            bundleUrl: bundleUrl,
+            runtimeUrl: URL(string: "http://localhost:8081")!,
+            warningsFilter: .none,
+            isDeveloperApp: true,
+            clearDataAtLaunch: false,
+            splashScreenPresenter: nil,
+            reactLoading: nil,
+            enableThreeFingerGestures: false
+        )
+        AppPreferences.devModeEnabled = true
+        AppPreferences.remoteDebuggingEnabled = true
+        ReactNative.shared.setup(mendixApp)
+        ReactNative.shared.start()
+    }
+
+    func bundleURL() -> URL? {
+        #if DEBUG
+            RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+        #else
+            Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+        #endif
+    }
+}
