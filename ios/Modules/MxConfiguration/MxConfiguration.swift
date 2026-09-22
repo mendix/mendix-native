41d2b150770fe50eb430e82373d600ae0295b3b3
Yogendra Shelke <yogendrashelke1992@gmail.com>
2026-09-22 16:26:07 +0530

    Update MxConfiguration.swift

--- ios/Modules/MxConfiguration/MxConfiguration.swift
+++ ios/Modules/MxConfiguration/MxConfiguration.swift
@@ -3,7 +3,7 @@ import Foundation
 @objcMembers
 public class MxConfiguration: NSObject {
     
-    static let nativeBinaryVersion: Int = 40
+    public static let nativeBinaryVersion: Int = 40
     private static let defaultDatabaseName = "default"
     private static let defaultFilesDirectoryName = "files/default"
     
